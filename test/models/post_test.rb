52866ce88a66e622266380e8e0efd5f377458fb7
Askarbek Karasasev <askar.karasaev@gmail.com>
2016-04-13 13:10:02 +0900

    Post model test updated

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -5,8 +5,8 @@ class PostTest < ActiveSupport::TestCase
   test "post attributes must not be empty" do
     post = Post.new
     assert post.invalid?
-    assert post.errors[:title].any?
-    assert post.errors[:content].any?
+    assert post.errors[:title].any?, 'Post invalid without title'
+    assert post.errors[:content].any?, 'Post invalid without content'
   end
 
 end
